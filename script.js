11acee326bf5f73de329f77fd391430cf2430b3d
github_cristina-altreche <c.altreche@gmail.com>
2020-04-06 20:17:12 -0500

    Side nav updated js script

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
 function openNav() {
-    document.getElementById("sideNav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("sideNav").style.width = "200px";
+  document.getElementById("main").style.marginRight = "-350px";
 }
 
 function closeNav() {
-    document.getElementById("sideNav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("sideNav").style.width = "0";
+  document.getElementById("main").style.marginRight = "0";
 }
+
+function dropDown() {
+  var click = document.getElementById("myDropdown");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+
+function dropDown2() {
+  var click = document.getElementById("myDropdown2");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+
+/*function dropDown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function dropDown2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};*/
