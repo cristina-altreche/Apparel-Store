61a893433636a5d5456f231480d69d1be0c50043
github_cristina-altreche <c.altreche@gmail.com>
2020-04-12 11:58:21 -0500

    Added women's & menspages

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-function openNav() {
-  document.getElementById("sideNav").style.width = "200px";
-  document.getElementById("main").style.marginRight = "-350px";
-}
-
-function closeNav() {
-  document.getElementById("sideNav").style.width = "0";
-  document.getElementById("main").style.marginRight = "0";
-}
-
-function dropDown() {
-  var click = document.getElementById("myDropdown");
-  if (click.style.display === "none") {
-    click.style.display = "block";
-  } else {
-    click.style.display = "none";
-  }
-}
-
-function dropDown2() {
-  var click = document.getElementById("myDropdown2");
-  if (click.style.display === "none") {
-    click.style.display = "block";
-  } else {
-    click.style.display = "none";
-  }
-}
-
 /*Bootstrap JS Below*/
 $(document).ready(function () {
   var fixHeight = function () {
